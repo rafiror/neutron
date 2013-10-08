37e787fcbb57a9b5b9a196635c00a9a98d19316c
Hirofumi Ichihara <mrkaeru@Hirofumi-Ichihara-no-iMac.local>
2013-10-09 00:51:10 +0900

    modify test of admin_state_up=false. I enable test to confirm that delete of namespace is executed

--- neutron/tests/unit/test_l3_agent.py
+++ neutron/tests/unit/test_l3_agent.py
@@ -516,13 +516,6 @@ class TestBasicRouterOperations(base.BaseTestCase):
 
 
     def test_routers_with_admin_state_down(self):
-        class FakeDev(object):
-            def __init__(self, name):
-                self.name = name
-
-        self.mock_ip.get_namespaces.return_value = ['qrouter-foo']
-        self.mock_ip.get_devices.return_value = [FakeDev('qr-aaaa'),
-                                                 FakeDev('qgw-aaaa')]
 
         agent = l3_agent.L3NATAgent(HOSTNAME, self.conf)
         self.plugin_api.get_external_network_id.return_value = None
@@ -531,12 +524,11 @@ class TestBasicRouterOperations(base.BaseTestCase):
             {'id': _uuid(),
              'admin_state_up': False,
              'external_gateway_info': {}}]
-        agent._router_removed = mock.MagicMock()
         agent._destroy_router_namespace = mock.MagicMock()
+        agent.process_router = mock.MagicMock()
+        agent._router_added(routers[0]['id'], routers[0])
         agent._process_routers(routers)
-        self.assertNotIn(routers[0]['id'], agent.router_info)
-        self.assertEqual(agent._router_removed.call_count, 1)
-        #self.assertEqual(agent._destroy_router_namespace.call_count, 1)
+        self.assertEqual(agent._destroy_router_namespace.call_count, 1)
 
     def test_router_deleted(self):
         agent = l3_agent.L3NATAgent(HOSTNAME, self.conf)
